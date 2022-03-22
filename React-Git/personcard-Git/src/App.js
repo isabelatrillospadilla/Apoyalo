4b938ae32253a20f3cce4be112cd1930c28e190d
isabelatrillospadilla <97717766+isabelatrillospadilla@users.noreply.github.com>
2022-03-21 21:44:24 -0300

    tareas react

---
+++ React-Git/personcard-Git/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import PersonCard from './components/PersonCard';
+
+function App() {
+  return (
+    <div className="App">
+      <PersonCard lastName="Doe" firstName="Jane"  age={45} hairColor={"Black"}></PersonCard>
+      <PersonCard lastName="Smith" firstName="John"  age={88} hairColor={"Brown"}></PersonCard> 
+      <PersonCard lastName="Fillmore" firstName="Millard"  age={50} hairColor={"Brown"}></PersonCard>   
+      <PersonCard lastName="Smith" firstName="Maria"  age={62} hairColor={"Brown"}></PersonCard>
+   
+    </div>
+  );
+}
+
+export default App;
